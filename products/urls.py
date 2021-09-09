1c304ce81d65ab79d95e230d330aaae87d71f353
wlgns410 <wlgns410@gmail.com>
2021-09-09 20:14:16 +0900

    Fix review url

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from products.views import ReviewList
 
 urlpatterns = [
-    path('review/', views.ReivewList.as_view()),
+    path('review', ReviewList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
