8550a1fc43eab23dc1b08078845becbe5661ed3d
wlgns410 <wlgns410@gmail.com>
2021-09-10 17:55:26 +0900

    delete url

--- products/urls.py
+++ products/urls.py
@@ -2,13 +2,6 @@
 from django.urls import path
 from products.views import ReviewList, CreateReviewList
 
-# schedule_detail = ReviewList.as_view({
-#     "get": "retrieve",
-#     "put": "update",
-#     "patch": "partial_update",
-#     "delete": "destroy",
-# })
-
 
 urlpatterns = [
     path('review/<int:review_id>', ReviewList.as_view({'get': 'list', 'delete' : 'destroy'})),
